20b9e3a1989e5a96e74703e7efb7c911a0d04c6b
Sang Dang <sang@sniffie.fi>
2020-05-19 21:28:18 +0300

    init

---
+++ index.ts
@@ -0,0 +1,11 @@
+const useTailwind = () => {
+  const initClass = '';
+
+  const makeClass = () => {};
+
+  console.log('holaa');
+
+  return [initClass, makeClass];
+};
+
+export default useTailwind;
